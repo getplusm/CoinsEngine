f57c062ad051755db9c27d46b74cd8b4d6085fdb
plazmer <48393153+getplusm@users.noreply.github.com>
2023-12-27 00:57:43 +0500

    1.0.2

--- src/main/java/t/me/p1azmer/engine/coins/command/currency/impl/FreeCommand.java
+++ src/main/java/t/me/p1azmer/engine/coins/command/currency/impl/FreeCommand.java
@@ -5,15 +5,11 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import t.me.p1azmer.engine.api.command.CommandResult;
 import t.me.p1azmer.engine.coins.CoinsEngine;
-import t.me.p1azmer.engine.coins.Placeholders;
 import t.me.p1azmer.engine.coins.api.currency.Currency;
 import t.me.p1azmer.engine.coins.command.currency.CurrencySubCommand;
 import t.me.p1azmer.engine.coins.config.Lang;
 import t.me.p1azmer.engine.coins.config.Perms;
-import t.me.p1azmer.engine.coins.data.impl.CurrencyData;
 import t.me.p1azmer.engine.utils.Colorizer;
-import t.me.p1azmer.engine.utils.EngineUtils;
-import t.me.p1azmer.plugin.gameui.GameUIAPI;
 
 public class FreeCommand extends CurrencySubCommand {
 
@@ -30,32 +26,32 @@ public class FreeCommand extends CurrencySubCommand {
             return;
         }
 
-        if (!EngineUtils.hasPlugin("GameUI") || GameUIAPI.PLUGIN == null) {
+//        if (!EngineUtils.hasPlugin("GameUI") || GameUIAPI.PLUGIN == null) {
             sender.sendMessage(Colorizer.apply("&cError when running command. Wait and try again"));
             return;
-        }
+//        }
 
-        this.plugin.getUserManager().getUserDataAsync(player.getName()).thenAccept(user -> {
-            if (user == null) {
-                this.errorPlayer(sender);
-                return;
-            }
-
-            if (GameUIAPI.PLUGIN.getUserManager().checkCommandCooldown(player, "/" + this.getLabelFull())) {
-                player.sendMessage("cd\n"+this.getLabelFull());
-                return;
-            }
-
-            CurrencyData data = user.getCurrencyData(this.currency);
-            data.addBalance(this.currency.getFreeValue());
-            this.plugin.getMessage(Lang.CURRENCY_CURRENCY_FREE_SUCCESS)
-                    .replace(this.currency.replacePlaceholders())
-                    .replace(Placeholders.PLAYER_NAME, user.getName())
-                    .replace(Placeholders.GENERIC_AMOUNT, currency.format(this.currency.getFreeValue()))
-                    .send(sender);
-        }).exceptionally(throwable -> {
-            sender.sendMessage(throwable.getMessage());
-            return null;
-        });
+//        this.plugin.getUserManager().getUserDataAsync(player.getName()).thenAccept(user -> {
+//            if (user == null) {
+//                this.errorPlayer(sender);
+//                return;
+//            }
+//
+//            if (GameUIAPI.PLUGIN.getUserManager().checkCommandCooldown(player, "/" + this.getLabelFull())) {
+//                player.sendMessage("cd\n"+this.getLabelFull());
+//                return;
+//            }
+//
+//            CurrencyData data = user.getCurrencyData(this.currency);
+//            data.addBalance(this.currency.getFreeValue());
+//            this.plugin.getMessage(Lang.CURRENCY_CURRENCY_FREE_SUCCESS)
+//                    .replace(this.currency.replacePlaceholders())
+//                    .replace(Placeholders.PLAYER_NAME, user.getName())
+//                    .replace(Placeholders.GENERIC_AMOUNT, currency.format(this.currency.getFreeValue()))
+//                    .send(sender);
+//        }).exceptionally(throwable -> {
+//            sender.sendMessage(throwable.getMessage());
+//            return null;
+//        });
     }
 }
